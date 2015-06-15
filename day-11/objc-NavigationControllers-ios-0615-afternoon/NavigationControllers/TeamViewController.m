5668b8ba6381bb065934e9fb2d7eb93048bac764
Tim Clem <tim.clem@gmail.com>
2015-06-15 16:38:25 -0400

    Table views intro code

--- day-11/objc-NavigationControllers-ios-0615-afternoon/NavigationControllers/TeamViewController.m
+++ day-11/objc-NavigationControllers-ios-0615-afternoon/NavigationControllers/TeamViewController.m
@@ -9,8 +9,14 @@
 #import "TeamViewController.h"
 #import "TeamDetailViewController.h"
 #import "TeamMember.h"
+#import "TeamMemberButton.h"
 
 @interface TeamViewController ()
+
+@property (weak, nonatomic) IBOutlet TeamMemberButton *alButton;
+@property (weak, nonatomic) IBOutlet TeamMemberButton *joeButton;
+@property (weak, nonatomic) IBOutlet TeamMemberButton *chrisButton;
+@property (weak, nonatomic) IBOutlet TeamMemberButton *aviButton;
 
 @end
 
@@ -19,17 +25,16 @@
 -(void)viewDidLoad
 {
     [super viewDidLoad];
-}
 
--(TeamMember *)teamMemberForSegueIdentifier:(NSString *)identifier
-{
+    TeamMember *joe = [[TeamMember alloc] initWithName:@"Joe" image:[UIImage imageNamed:@"joe.jpg"]];
+    TeamMember *chris = [[TeamMember alloc] initWithName:@"Chris" image:[UIImage imageNamed:@"chris.jpg"]];
+    TeamMember *avi = [[TeamMember alloc] initWithName:@"Avi" image:[UIImage imageNamed:@"avi.jpg"]];
+    TeamMember *al = [[TeamMember alloc] initWithName:@"Al" image:[UIImage imageNamed:@"al.jpg"]];
 
-    NSDictionary *teamMembers = @{ @"joe": joe,
-                                   @"chris": chris,
-                                   @"avi": avi,
-                                   @"al": al };
-
-    return teamMembers[identifier];
+    self.alButton.teamMember = al;
+    self.joeButton.teamMember = joe;
+    self.chrisButton.teamMember = chris;
+    self.aviButton.teamMember = avi;
 }
 
 #pragma mark - Navigation
@@ -37,9 +42,10 @@
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     TeamDetailViewController *destination = segue.destinationViewController;
-    TeamMember *tappedTeamMember = [self teamMemberForSegueIdentifier:segue.identifier];
+    TeamMemberButton *tappedButton = sender;
+//    TeamMember *tappedTeamMember = [self teamMemberForSegueIdentifier:segue.identifier];
 
-    destination.teamMember = tappedTeamMember;
+    destination.teamMember = tappedButton.teamMember;
 }
 
 
