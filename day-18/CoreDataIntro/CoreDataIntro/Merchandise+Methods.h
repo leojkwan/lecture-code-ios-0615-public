7f74fff6bd5a03f85fddd47a729934fff43e9475
Tim Clem <tim.clem@gmail.com>
2015-06-24 17:02:26 -0400

    Core data intro lecture

---
+++ day-18/CoreDataIntro/CoreDataIntro/Merchandise+Methods.h
@@ -0,0 +1,15 @@
+//
+//  Merchandise+Methods.h
+//  CoreDataIntro
+//
+//  Created by Timothy Clem on 6/24/15.
+//  Copyright (c) 2015 Flatiron School. All rights reserved.
+//
+
+#import "Merchandise.h"
+
+@interface Merchandise (Methods)
+
+-(NSString *)formattedPrice;
+
+@end
