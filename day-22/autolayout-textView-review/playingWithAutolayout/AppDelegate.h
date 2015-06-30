b7bf6477dcf42adff0c99d94acb62a34dff32296
TOMalley104 <tom@flatironschool.com>
2015-06-30 14:56:49 -0400

    add lab review autolayout-textViews

---
+++ day-22/autolayout-textView-review/playingWithAutolayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  playingWithAutolayout
+//
+//  Created by Tom OMalley on 3/2/15.
+//  Copyright (c) 2015 tomalley104. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
