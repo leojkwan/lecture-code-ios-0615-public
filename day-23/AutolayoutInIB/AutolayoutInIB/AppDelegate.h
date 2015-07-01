752a799616b7b9e8d6e038fd03fd58449c87644d
Tim Clem <tim.clem@gmail.com>
2015-07-01 15:02:34 -0400

    Autolayout in IB code

---
+++ day-23/AutolayoutInIB/AutolayoutInIB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutolayoutInIB
+//
+//  Created by Timothy Clem on 7/1/15.
+//  Copyright (c) 2015 Flatiron School. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
