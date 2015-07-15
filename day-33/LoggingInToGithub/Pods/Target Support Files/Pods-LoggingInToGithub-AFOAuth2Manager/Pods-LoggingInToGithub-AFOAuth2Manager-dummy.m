a74ed0ccea38058427dd25b3d788fa2e480647c2
Tim Clem <tim.clem@gmail.com>
2015-07-15 12:41:57 -0400

    NSNotificationCenter lecture code

---
+++ day-33/LoggingInToGithub/Pods/Target Support Files/Pods-LoggingInToGithub-AFOAuth2Manager/Pods-LoggingInToGithub-AFOAuth2Manager-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LoggingInToGithub_AFOAuth2Manager : NSObject
+@end
+@implementation PodsDummy_Pods_LoggingInToGithub_AFOAuth2Manager
+@end
